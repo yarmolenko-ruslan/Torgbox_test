6575d959b9e388e3fe102783285f5b7e3508a967
Yarmolenko Ruslan <yarm.rus@yandex.ru>
2025-03-10 18:07:51 +0700

    Revert "Добавить готовый проект в GitHub репозиторий"
    This reverts commit 2b20ac61343c7a2c1e756787bd2836ef92f70b8d.

--- src/components/CityName/CityName.tsx
+++
@@ -1,6 +0,0 @@
-import { ClockProps } from 'types/types';
-import styles from './cityName.module.scss';
-
-export const CityName = ({ clock }: ClockProps) => {
-	return <p className={styles.city}>{clock.timezone.city}</p>;
-};
